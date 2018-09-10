9dc78b81de6e5c3f544ba8bfe61da88df5c755e4
Азизов Тимур <timmik94@gmail.com>
2018-09-11 00:48:54 +0300

    Add Parser to parse commands in console and unit tests to it. Remove old commands.

---
+++ GitHubHelper/Commands/CommandData.cs
@@ -0,0 +1,25 @@
+﻿namespace GitHubHelper.Commands
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Data of the command from console;
+    /// </summary>
+    public class CommandData
+    {
+        /// <summary>
+        /// Gets or sets type of data that command works with.
+        /// </summary>
+        public string DataType { get; set; }
+
+        /// <summary>
+        /// Gets or sets Action of the command.
+        /// </summary>
+        public string Action { get; set; }
+
+        /// <summary>
+        /// Gets or sets dictionary of parameters with long names.
+        /// </summary>
+        public Dictionary<string, string> Parameters { get; set; }
+    }
+}
---
+++ GitHubHelper/Commands/CommandParser.cs
@@ -0,0 +1,59 @@
+﻿namespace GitHubHelper.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Parser of the console commands.
+    /// </summary>
+    public class CommandParser
+    {
+        /// <summary>
+        /// Reg expression of parameter name.
+        /// </summary>
+        public const string ParamRegexp = "--?";
+
+        /// <summary>
+        /// Parses command.
+        /// </summary>
+        /// <param name="command">Command string.</param>
+        /// <returns>Data of the command.</returns>
+        public static CommandData ParseCommand(string command)
+        {
+            string[] commandParts = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            CommandData data = new CommandData();
+            data.DataType = commandParts[0];
+            data.Action = commandParts[1];
+            data.Parameters = new Dictionary<string, string>();
+            for (int i = 2; i < commandParts.Length; i++)
+            {
+                Regex param = new Regex(CommandParser.ParamRegexp);
+                bool isParamName = param.IsMatch(commandParts[i]);
+                commandParts[i] = commandParts[i].Replace("-", string.Empty);
+                if (isParamName)
+                {
+                    // If parameter name is last in the string his value is empty.
+                    if (i + 1 == commandParts.Length)
+                    {
+                        data.Parameters.Add(commandParts[i], string.Empty);
+                    }
+                    else
+                    {
+                        bool isNextElementParam = param.IsMatch(commandParts[i + 1]);
+                        if (isNextElementParam)
+                        {
+                            data.Parameters.Add(commandParts[i], string.Empty);
+                        }
+                        else
+                        {
+                            data.Parameters.Add(commandParts[i], commandParts[i + 1]);
+                        }
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
